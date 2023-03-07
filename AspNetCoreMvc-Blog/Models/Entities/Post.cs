8ee523b38069b84466efd63e4aa6d354f9fd1cd4
Fernando Di Nardo Lazarin <dinardo.lazarin@gmail.com>
2023-03-07 15:12:01 -0300

    Criado Models/Entities "Post.cs"

---
+++ AspNetCoreMvc-Blog/Models/Entities/Post.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreMvc_Blog.Models.Entities
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Permalink { get; set; }
+        public string Title { get; set; }
+        public string Summary { get; set; }
+        public string Content { get; set; }
+        public DateTime PublishedOn { get; set; }
+
+        public Post()
+        {
+            PublishedOn = DateTime.Now;
+        }
+    }
+}
