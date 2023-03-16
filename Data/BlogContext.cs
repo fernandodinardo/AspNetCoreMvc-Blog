b148d3e91ec866710c293919ba4f3ee1a23de4b3
Fernando Di Nardo Lazarin <dinardo.lazarin@gmail.com>
2023-03-16 10:36:09 -0300

    Add project files.

---
+++ Data/BlogContext.cs
@@ -0,0 +1,22 @@
+﻿using AspNetCore_Blog.Data.Mapping;
+using AspNetCoreMvc_Blog.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Entity;
+using DbContext = System.Data.Entity.DbContext;
+
+namespace AspNetCore_Blog.Data
+{
+    public class BlogContext : DbContext
+    {
+        public System.Data.Entity.DbSet<Post> Posts { get; set; }
+
+        public BlogContext() : base("MsSqlServer")
+        {
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new PostMap());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Data/Mapping/PostMap.cs
@@ -0,0 +1,34 @@
+﻿using AspNetCoreMvc_Blog.Models.Entities;
+using System.Data.Entity.ModelConfiguration;
+
+namespace AspNetCore_Blog.Data.Mapping
+{
+    public class PostMap : EntityTypeConfiguration<Post>
+    {
+        public PostMap()
+        {
+            ToTable("Posts");
+            HasKey(x => x.Id);
+
+            Property(x => x.Permalink)
+                .HasMaxLength(100)
+                .IsVariableLength()
+                .IsRequired();
+
+            Property(x => x.Title)
+                .HasMaxLength(70)
+                .IsVariableLength()
+                .IsRequired();
+
+            Property(x => x.Summary)
+                .HasMaxLength(500)
+                .IsVariableLength()
+                .IsRequired();
+
+            Property(x => x.Content)
+                .IsMaxLength()
+                .IsVariableLength()
+                .IsOptional();
+        }
+    }
+}
